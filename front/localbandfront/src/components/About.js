c514d32d15798e05635e3a613975beb22708f075
Masavain <matti.vainionpaa@helsinki.fi>
2018-05-04 17:16:43 +0300

    heroku login toimii ja About-sivulla nyt selite sivuston toiminnasta

--- front/localbandfront/src/components/About.js
+++ front/localbandfront/src/components/About.js
@@ -3,20 +3,29 @@ import { connect } from 'react-redux'
 
 const About = () => {
   const style = {
-    padding: 10,
+    padding: 30,
     height: '1000px'
   }
   return(
     <div style={style}>
       <p>
-    Lorem ipsum dolor sit amet, consectetur adipisci elit,
-    sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquid ex ea commodi consequat.
-    Quis aute iure reprehenderit in voluptate velit esse cillum
-    dolore eu fugiat nulla pariatur. Excepteur sint obcaecat
-    cupiditat non proident, sunt in culpa qui officia deserunt
-    mollit anim id est laborum.
+        {`Welcome to LocalBands web app, a sort-of social media for new, uprising, 
+      underground bands and music nerds who want to find out about and share said bands and 
+      their music. I created the app as a part of the FullStack web app design module 
+      in the University of Helsinki in the spring of 2018. An integral part of 
+      the project was to pick a subject-matter that was significant for me and also 
+      so I could have a platform to satisfy my need to write and talk about music that I love.`}</p>
+
+      <p>{`Every user can create a profile to manage their favorites and to create 
+      band-specific pages for each of their musical projects and.
+      I decided to give the users a possibility to embed Bandcamp and YouTube 
+      urls as I feel they are few of the most important 
+      platforms for music sharing these days. An added Spotify-player is next in line. 
+      Bands can also make an image gallery with album art and/or photos from live shows as
+      well as a discography list of releases with info and a Bandcamp link.
+      `}
+      </p>
+      <p>{'LocalBands is totally free to use and there are no ads.'}
       </p>
     </div>
   )
